7fdb0838645a287e8fcdd240be8a3395c180c72a
ductranprof99 <duc.tranhvhk@hcmut.edu.vn>
2021-07-12 11:07:00 +0700

    start first period

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+1. run lib init.py to create profile folder and run bash shell
+? do i need to add custom chrome file path or i just make it static for just myself
--- test.py
+++ test.py
@@ -1,17 +1,19 @@
-# import mechanize
-# browser = mechanize.Browser()
-# browser.set_handle_robots(False)
-# cookies = mechanize.CookieJar()
-# browser.set_cookiejar(cookies)
-# browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
-# browser.set_handle_refresh(False)
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 
-# url = 'http://www.facebook.com/login.php'
-# browser.open(url)
-# browser.select_form(nr = 0)       #This is login-password form -> nr = number = 0
-# browser.form['email'] = "sdt"
-# browser.form['pass'] = "pass"
-# response = browser.submit()
-# print(response.read())
+def open_instagram():    
+    try:
+        # Connect
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--incognito")
+        global browser # this will prevent the browser variable from being garbage collected
+        browser = webdriver.Chrome('./chromedriver.exe', chrome_options=chrome_options)
+        browser.set_window_size(1800, 900)
+        browser.get("https://www.instagram.com/accounts/login/?hl=de")
+        #browser.find_element(By.NAME, 'username').send_keys('MYEMAIL', Keys.TAB, 'MYPW', Keys.ENTER)
+    except Exception as e:
+        print (e, 'Instagram')
 
-from pyfacebook import GraphAPI
+open_instagram()
