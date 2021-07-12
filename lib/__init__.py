2371a41d405bb3c7ab57c238b629d20567ae09fa
ductranprof99 <duc.tranhvhk@hcmut.edu.vn>
2021-07-12 20:06:36 +0700

    step 2.2 access incomming data

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,4 +1,15 @@
-import subprocess,os,sys
+
+import subprocess,os,sys,json
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+
+
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "localhost:8000")
 
 if(not os.path.isdir('./profileFolder')):
     os.mkdir('./profileFolder')
@@ -13,3 +24,23 @@ if(os.path.isfile('./shellscript.ps1')):
               stdout=sys.stdout)
     p.communicate()
     
+scavenger = json.load(open('account.json'))
+
+def open_messenger(opt,iter):    
+    try:
+        # Connect
+        
+        global browser # this will prevent the browser variable from being garbage collected
+        browser = webdriver.Chrome('./chromedriver.exe', options=opt)
+        browser.set_window_size(1800, 900)
+        browser.get("https://www.messenger.com/login/")
+        nameField = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//input[@id="email"]')))
+        nameField.send_keys(iter["username"],Keys.TAB,iter["password"],Keys.ENTER)
+        # while(True):
+        #     incomming_current_text_data = browser.find_element(By.XPATH,'//div[@data-testid="messenger_incoming_text_row"]//span//div//div[@dir=auto]') 
+        #     print(incomming_current_text_data)
+    except Exception as e:
+        print (e, 'messenger')
+
+open_messenger(chrome_options,scavenger)
+
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-from os import name
+import subprocess,os,sys,json
+from time import sleep
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,26 +8,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 
 
-chrome_options = Options()
-chrome_options.add_experimental_option("debuggerAddress", "localhost:8000")
+opt = Options()
+opt.add_experimental_option("debuggerAddress", "localhost:8000")
 
-def open_messenger(opt):    
-    try:
-        # Connect
-        
-        global browser # this will prevent the browser variable from being garbage collected
-        browser = webdriver.Chrome('./chromedriver.exe', options=opt)
-        browser.set_window_size(1800, 900)
-        browser.get("https://www.messenger.com/login/")
-        WebDriverWait(browser, 5)
-        nameField = browser.find_element_by_xpath('//input[@id="email"]')
-        passwordField = browser.find_element_by_xpath('//input[@id="pass"]')
-        nameField.send_keys('email')
-        passwordField.send_keys('passwd',Keys.ENTER)
-        # browser.send_keys('0918139331',Keys.TAB)
-        # for i in listTag:
-        #     print(i)
-    except Exception as e:
-        print (e, 'messenger')
 
-open_messenger(chrome_options)
+browser = webdriver.Chrome('./chromedriver.exe', options=opt)
+
+browser.get("https://www.messenger.com/t/*khach_id")
+while(True):
+    sleep(1)
+    section_container =  browser.find_elements_by_xpath('//div[@data-testid="incoming_group"]')
+    section_container =  browser.find_elements_by_xpath('//div[@data-testid="outgoing_group"]')
+    for i in section_container:
+        print(i.get_attribute('outerText'))
+    
